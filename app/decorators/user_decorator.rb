df4b99929ffdf61b01b5a807a9ceedf9874d9322
Marin Okado <marinokado@MarinnoMacBook-Pro.local>
2024-06-13 01:48:19 +0900

    RUNTEQの文字を追加

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -5,13 +5,3 @@ class UserDecorator < Draper::Decorator
     "#{object.last_name} #{object.first_name}"
   end
 end
-
-  # Define presentation-specific methods here. Helpers are accessed through
-  # `helpers` (aka `h`). You can override attributes, for example:
-  #
-  #   def created_at
-  #     helpers.content_tag :span, class: 'time' do
-  #       object.created_at.strftime("%a %m/%d/%y")
-  #     end
-  #   end
-
