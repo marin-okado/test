ebd1f196a2dcff364c5612dbd98a50c6cfe6cda7
Marin Okado <marinokado@MarinnoMacBook-Pro.local>
2024-07-11 15:45:06 +0900

    Initial commit

--- db/migrate/20240615134341_add_board_image_to_boards.rb
+++
@@ -1,5 +0,0 @@
-class AddBoardImageToBoards < ActiveRecord::Migration[7.0]
-  def change
-    add_column :boards, :board_image, :string
-  end
-end
--- db/migrate/20240618164603_drop_table.rb
+++
@@ -1,5 +0,0 @@
-class DropTable < ActiveRecord::Migration[7.0]
-  def change
-    drop_table :bookmarks
-  end
-end
